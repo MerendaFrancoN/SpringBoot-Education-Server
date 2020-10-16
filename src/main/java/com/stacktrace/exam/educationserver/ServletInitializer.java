e6a001be9aed818977d8e6da016e95ea11b79d24
stormtrooper <merenda.franco83@gmail.com>
2020-10-16 13:05:47 -0300

    First commit - starting point

---
+++ src/main/java/com/stacktrace/exam/educationserver/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.stacktrace.exam.educationserver;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EducationServerApplication.class);
+	}
+
+}
