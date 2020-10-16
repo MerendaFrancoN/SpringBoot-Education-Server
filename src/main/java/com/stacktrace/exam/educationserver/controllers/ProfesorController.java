204cde93ca8a76c65ae7f04f3adf658a69806caa
stormtrooper <merenda.franco83@gmail.com>
2020-10-16 17:38:31 -0300

    Added error ResponseError

--- src/main/java/com/stacktrace/exam/educationserver/controllers/ProfesorController.java
+++ src/main/java/com/stacktrace/exam/educationserver/controllers/ProfesorController.java
@@ -1,11 +1,10 @@
 package com.stacktrace.exam.educationserver.controllers;
 
-import com.stacktrace.exam.educationserver.entities.Curso;
 import com.stacktrace.exam.educationserver.entities.Profesor;
-import com.stacktrace.exam.educationserver.service.CursoService;
 import com.stacktrace.exam.educationserver.service.ProfesorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -18,16 +17,16 @@ public class ProfesorController {
     @Autowired
     ProfesorService profesorService;
 
-    @GetMapping(value = "/profesor")
+    @GetMapping(value = "/profesores")
     @ResponseBody
     @ResponseStatus(HttpStatus.OK)
     public List<Profesor> getAll(){
         return profesorService.getAll();
     }
 
-    @PostMapping(value = "/profesor")
+    @PostMapping(value = "/profesores")
     @ResponseStatus(HttpStatus.CREATED)
-    public Object createClient(@RequestBody Profesor profesor) {
+    public Object createProfesor(@RequestBody Profesor profesor) {
         Map<String, Object> mapResponse = new HashMap<>();
         mapResponse.put("id", profesorService.saveProfesor(profesor).getId());
         return  mapResponse;
