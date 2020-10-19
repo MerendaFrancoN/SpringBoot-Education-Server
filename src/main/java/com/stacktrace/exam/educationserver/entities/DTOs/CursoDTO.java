a5e2f4c274e9d4b82b8214f27a31493e40c74c20
stormtrooper <merenda.franco83@gmail.com>
2020-10-18 21:30:11 -0300

    Remove notas from cursoDTO
    - Unused attribute through json, use /notas to crud notas for alumnos-cursos

--- src/main/java/com/stacktrace/exam/educationserver/entities/DTOs/CursoDTO.java
+++ src/main/java/com/stacktrace/exam/educationserver/entities/DTOs/CursoDTO.java
@@ -2,7 +2,6 @@ package com.stacktrace.exam.educationserver.entities.DTOs;
 
 import com.stacktrace.exam.educationserver.entities.Alumno;
 import com.stacktrace.exam.educationserver.entities.Curso;
-import com.stacktrace.exam.educationserver.entities.Nota;
 
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -18,7 +17,6 @@ public class CursoDTO {
     private double nota_aprobacion;
     private Set<String> alumnos_dni = new HashSet<>();
     private String profesor_dni;
-    private List<Long> notas_id = new LinkedList<>();
 
     public CursoDTO(){}
     public CursoDTO(Curso curso){
@@ -29,10 +27,6 @@ public class CursoDTO {
         this.setNota_aprobacion(curso.getNota_aprobacion());
         this.setAlumnos_dni(curso.getAlumnosEnlistados().stream()
                 .map(Alumno::getDni).collect(Collectors.toSet()));
-
-        if(curso.getLista_notas() != null)
-            this.setNotas_id(curso.getLista_notas().stream()
-                    .map(Nota::getId).collect(Collectors.toList()));
 
         try{
             if(curso.getDictadoPor().getDni().equals(""))
@@ -99,13 +93,5 @@ public class CursoDTO {
 
     public void setProfesor_dni(String profesor_dni) {
         this.profesor_dni = profesor_dni;
-    }
-
-    public List<Long> getNotas_id() {
-        return notas_id;
-    }
-
-    public void setNotas_id(List<Long> notas_id) {
-        this.notas_id = notas_id;
     }
 }
